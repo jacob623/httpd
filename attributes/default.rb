14cec8bf1b5ff9d35b9cbf9372fc389bded1562a
jacob623 <jacob.a.long@gmail.com>
2017-03-29 16:32:15 -0500

    Add ubuntu support

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,11 @@
-default['httpd']['package_name'] = 'httpd'
+
+case node['platform']
+when 'ubuntu'
+  default['httpd']['package_name'] = 'apache2'
+  default['httpd']['service_name'] = 'apache2'
+else
+  default['httpd']['package_name'] = 'httpd'
+  default['httpd']['service_name'] = 'httpd'
+end
+
 default['web']['index_file'] = '/var/www/html/index.html'
-default['httpd']['service_name'] = 'httpd'
