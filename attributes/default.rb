f14a05be2073884bb06cd01e9e153b2f7ebb78ce
jacob623 <jacob.a.long@gmail.com>
2017-03-29 15:43:47 -0500

    moving more to attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,3 @@
 default['httpd']['package_name'] = 'httpd'
+#default['web']['index_file'] = '/var/www/html/index.html'
+#default['httpd']['service_name'] = 'httpd'
--- recipes/configuration.rb
+++ recipes/configuration.rb
@@ -4,6 +4,6 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-file '/var/www/html/index.html' do
+file node['web']['index_file'] do
   content '<h1>Welcome Home!</h1>'
 end
