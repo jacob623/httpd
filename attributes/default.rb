21b2900d28659cb24f9fa970de9e741707ecd981
jacob623 <jacob.a.long@gmail.com>
2017-03-29 16:33:50 -0500

    restore attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
 
-#case node['platform']
-#when 'ubuntu'
-#  default['httpd']['package_name'] = 'apache2'
-#  default['httpd']['service_name'] = 'apache2'
-#else
+case node['platform']
+when 'ubuntu'
+  default['httpd']['package_name'] = 'apache2'
+  default['httpd']['service_name'] = 'apache2'
+else
   default['httpd']['package_name'] = 'httpd'
   default['httpd']['service_name'] = 'httpd'
-#end
+end
 
 default['web']['index_file'] = '/var/www/html/index.html'
