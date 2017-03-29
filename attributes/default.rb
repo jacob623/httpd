2bb93e071941a97b915095338f9cf246cc108ebc
jacob623 <jacob.a.long@gmail.com>
2017-03-29 15:30:47 -0500

    adding pry

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,1 @@
-default['httpd']['package_name'] = 'httpd'
+default['htpd']['package_name'] = 'httpd'
--- recipes/install.rb
+++ recipes/install.rb
@@ -4,4 +4,7 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+require 'pry'
+binding.pry
+
 package node['httpd']['package_name']
